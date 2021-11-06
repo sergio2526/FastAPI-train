dbd3329f64535817d7c49306b017713b07dad007
sergio2526 <sergiorubiano25@hotmail.com>
2021-11-06 17:49:46 -0500

    Agregando archivos

--- main.py
+++ main.py
@@ -1,79 +1,146 @@
-
 #Python
-
 from typing import Optional
-from fastapi.datastructures import Default
-from fastapi.param_functions import Path, Query
+from enum import Enum
+from fastapi.param_functions import Form
 
 #Pydantic
 from pydantic import BaseModel
+from pydantic import Field
 
 #FastAPI
 from fastapi import FastAPI
-from fastapi import Body, Query, Path
+from fastapi import status
+from fastapi import Body, Query, Path, Form
+from starlette.status import HTTP_201_CREATED
 
 app = FastAPI()
 
+# Models
 
-#Models
-class Person(BaseModel):
-    first_name: str
-    last_name: str
-    age: int
-    hair_color: Optional[str] = None
-    is_married: Optional[bool] = None
+class HairColor(Enum): 
+    white = "white"
+    brown = "brown"
+    black = "black"
+    blonde = "blonde"
+    red = "red"
 
-    #Ejemplo por defecto en el body
-    class Config:
-        schema_extra = {
-            "example":{
-                "first_name":"sergio",
-                "last_name": "Rubiano",
-                "age":17,
-                "hair_color":"black",
-                "is_married":True
-            }
-        }
+class Location(BaseModel): 
+    city: str
+    state: str
+    country: str
 
-@app.get("/")
-def home():
-    return {
-        "Hello":"world"
-    }
+class PersonBase(BaseModel):
+    first_name: str = Field(
+        ..., 
+        min_length=1,
+        max_length=50,
+        example="Miguel"
+        )
+    last_name: str = Field(
+        ..., 
+        min_length=1,
+        max_length=50,
+        example="Torres"
+        )
+    age: int = Field(
+        ...,
+        gt=0,
+        le=115,
+        example=25
+    )
+    hair_color: Optional[HairColor] = Field(default=None, example=HairColor.black)
+    is_married: Optional[bool] = Field(default=None, example=False)
 
+class Person(PersonBase): 
+    password: str = Field(..., min_length=8)
 
-@app.post("/person/new")
-def create_person(person: Person = Body(...)):
+    # class Config: 
+    #     schema_extra = {
+    #         "example": {
+    #             "first_name": "Facundo",
+    #             "last_name": "García Martoni",
+    #             "age": 21, 
+    #             "hair_color": "blonde",
+    #             "is_married": False
+    #         }
+    #     }
+
+class PersonOut(PersonBase): 
+    pass
+
+class LoginOut(BaseModel):
+    username: str = Field(..., max_length=20, example="miguel2021")
+    message: str = Field(default="Login Succesfully!")
+    
+@app.get(path="/", status_code=status.HTTP_200_OK)
+def home(): 
+    return {"Hello": "World"}
+
+# Request and Response Body
+
+@app.post(path="/person/new", response_model=PersonOut, status_code=status.HTTP_201_CREATED)
+def create_person(person: Person = Body(...)): 
     return person
 
+# Validaciones: Query Parameters
 
-#Query Parameters
-
-@app.get("/person/detail")
+@app.get(path="/person/detail", status_code=status.HTTP_200_OK)
 def show_person(
     name: Optional[str] = Query(
-        None, 
+        None,
         min_length=1, 
         max_length=50,
         title="Person Name",
-        description="This is the person name. It's between 1 and 50 characters"
+        description="This is the person name. It's between 1 and 50 characters",
+        example="Rocío"
         ),
     age: str = Query(
         ...,
         title="Person Age",
-        description="This is the person age. It's required"
-    )
-):
-    return {name : age}
+        description="This is the person age. It's required",
+        example=25
+        )
+): 
+    return {name: age}
 
+# Validaciones: Path Parameters
 
-# Validaciones: Path parameters
-@app.get("/person/detail/{person_id}")
+@app.get(path="/person/detail/{person_id}",status_code=status.HTTP_200_OK)
 def show_person(
     person_id: int = Path(
-        ...,
-        gt=0
-        ),
-):
+        ..., 
+        gt=0,
+        example=123
+        )
+): 
     return {person_id: "It exists!"}
 
+# Validaciones: Request Body
+
+@app.put(path="/person/{person_id}",status_code=status.HTTP_200_OK)
+def update_person(
+    person_id: int = Path(
+        ...,
+        title="Person ID",
+        description="This is the person ID",
+        gt=0,
+        example=123
+    ),
+    person: Person = Body(...),
+    #location: Location = Body(...)
+): 
+    #results = person.dict()
+    #results.update(location.dict())
+    #return results
+    return person
+
+#Creando formulario
+@app.post(
+    path='/login',
+    response_model=LoginOut,
+    status_code=status.HTTP_200_OK
+)
+def login(username: str =  Form(...), password: str = Form(...)):
+    return LoginOut(username=username)
+
+# Cookies and Headers Parameters
