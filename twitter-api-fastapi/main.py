d0927623a341fab070c1b4e778d8170a8de71492
sergio2526 <sergiorubiano25@hotmail.com>
2021-11-15 16:35:45 -0500

    Model tweets

--- twitter-api-fastapi/main.py
+++ twitter-api-fastapi/main.py
@@ -2,7 +2,7 @@
 
 #Python
 from uuid import UUID
-from datetime import date
+from datetime import date , datetime
 from typing import Optional
 
 #Pydantic
@@ -43,6 +43,17 @@ class User(UserBase):
     )
     birth_date: Optional[date] =  Field(default=None)
 
+
+class Tweet(BaseModel):
+    tweet_id: UUID = Field(...)
+    content: str = Field(
+        ...,
+        min_length=1,
+        max_length=256
+    )
+    created_at: datetime = Field(default=datetime.now())
+    update_at: Optional[datetime] = Field(default=None)
+    by: User= Field(...)
 
 @app.get(
     path="/"
